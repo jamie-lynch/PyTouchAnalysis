2fd6d3a360aeb475bbfc05e6f072c803c94fa560
Jamie Lynch <github@jamielynch.co.uk>
2017-09-16 17:00:31 +0100

    finished the sliders including the logic

--- src/components/video/controls/TScrubSlider.py
+++ src/components/video/controls/TScrubSlider.py
@@ -9,12 +9,58 @@ Version 0.0
 from PySide import QtGui, QtCore
 
 
-class TScrubSlider(QtGui.QSlider):
+class TScrubSlider(QtGui.QWidget):
     """Custom slider used for scrubbing forwards and backwards through the video files"""
 
     def __init__(self, player):
         """Function to initialise the class"""
-        super(TScrubSlider, self).__init__(QtCore.Qt.Horizontal)
+        super(TScrubSlider, self).__init__()
+
+        # create attributes
+        self.slider = None
 
         # set a reference to the video
         self.player = player
+
+        # create the ui elements
+        self.init_ui()
+
+        # setup the slider logic
+        self.slider_setup()
+
+    def init_ui(self):
+        """Function which creates the UI elements"""
+
+        # Create a grid
+        grid = QtGui.QGridLayout()
+        self.setLayout(grid)
+
+        # Create a slider
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+
+        # Create the symbols
+        minus = QtGui.QLabel()
+        minus.setPixmap(QtGui.QPixmap('resources/icons/rewind.png'))
+        plus = QtGui.QLabel()
+        plus.setPixmap(QtGui.QPixmap('resources/icons/fast_forward.png'))
+
+        # Add it all to the grid
+        grid.addWidget(minus, 0, 0)
+        grid.addWidget(self.slider, 0, 1)
+        grid.addWidget(plus, 0, 2)
+
+    def slider_setup(self):
+        """Function which sets the slider logic"""
+        # set the range and starting value
+        self.slider.setRange(-10, 10)
+        self.slider.setValue(0)
+
+        # enable tracking so that the value can be tracked while the slider is dragged
+        self.slider.setTracking(True)
+
+        # when the slider is released then return to central
+        self.slider.sliderReleased.connect(lambda: self.slider.setValue(0))
+
+        # set the steps to zero (this stops people clicking on the bar to move the slider)
+        self.slider.setSingleStep(0)
+        self.slider.setPageStep(0)
--- src/components/video/controls/TSpeedSlider.py
+++ src/components/video/controls/TSpeedSlider.py
@@ -9,12 +9,49 @@ Version 0.0
 from PySide import QtGui, QtCore
 
 
-class TSpeedSlider(QtGui.QSlider):
+class TSpeedSlider(QtGui.QWidget):
     """Custom slider used for controlling the speed of playback"""
 
     def __init__(self, player):
         """Function to initialise the class"""
-        super(TSpeedSlider, self).__init__(QtCore.Qt.Horizontal)
+        super(TSpeedSlider, self).__init__()
+
+        # create attributes
+        self.slider = None
 
         # set a reference to the video
         self.player = player
+
+        # create the ui elements
+        self.init_ui()
+
+        # setup the slider
+        self.setup_slider()
+
+    def init_ui(self):
+        """Function which creates the UI elements"""
+
+        # Create a grid
+        grid = QtGui.QGridLayout()
+        self.setLayout(grid)
+
+        # Create a slider
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+
+        # Create the symbols
+        minus = QtGui.QLabel()
+        minus.setPixmap(QtGui.QPixmap('resources/icons/minus.png'))
+        plus = QtGui.QLabel()
+        plus.setPixmap(QtGui.QPixmap('resources/icons/plus.png'))
+
+        # Add it all to the grid
+        grid.addWidget(minus, 0, 0)
+        grid.addWidget(self.slider, 0, 1)
+        grid.addWidget(plus, 0, 2)
+
+    def setup_slider(self):
+        """Function which sets up the logic of the slider"""
+
+        # set the range and start value
+        self.slider.setRange(0, 10)
+        self.slider.setValue(10)
